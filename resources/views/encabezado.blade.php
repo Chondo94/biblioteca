4215dfadebdb1ceb79a3ad5ce27abc12fb424396
Chondo94 <estuardobc@outlook.es>
2019-11-26 23:16:33 -0600

    arregle el problema de estilo que tenia la parte de la sesion y registrarse

--- resources/views/encabezado.blade.php
+++ resources/views/encabezado.blade.php
@@ -47,51 +47,57 @@
         </div>
       </div>
     </div>
-    
+
     <div class="navbar-end">
       <div class="navbar-item">
         <div class="buttons">
-        <nav class="navbar has-shadow">
-                <div class="container">
-                    <div class="navbar-brand">
-                        <a href="{{ url('/libro') }}" class="navbar-item">{{ config('app.name', 'Laravel') }}</a>
+          <nav class="navbar is-dark">
+            <div class="container">
+              <div class="navbar-brand">
+                <a href="{{ url('/libro') }}" class="navbar-item">{{ config('app.name', 'Laravel') }}</a>
 
-                        <div class="navbar-burger burger" data-target="navMenu">
-                            <span></span>
-                            <span></span>
-                            <span></span>
-                        </div>
-                    </div>
-
-                    <div class="navbar-menu" id="navMenu">
-                        <div class="navbar-start"></div>
-
-                        <div class="navbar-end">
-                            @if (Auth::guest())
-                                <a class="navbar-item " href="{{ route('login') }}">Login</a>
-                                
-                            @else
-                                <div class="navbar-item has-dropdown is-hoverable">
-                                    <a class="navbar-link" href="#">{{ Auth::user()->name }}</a>
-                                    <a class="navbar-item " href="{{ route('register') }}">Register</a>
-                                    <div class="navbar-dropdown">
-                                        <a class="navbar-item" href="{{ route('logout') }}"
-                                           onclick="event.preventDefault();document.getElementById('logout-form').submit();">
-                                            Logout
-                                        </a>
-
-
-                                        <form id="logout-form" action="{{ route('logout') }}" method="POST"
-                                              style="display: none;">
-                                            {{ csrf_field() }}
-                                        </form>
-                                    </div>
-                                </div>
-                            @endif
-                        </div>
-                    </div>
+                <div class="navbar-burger burger" data-target="navMenu">
+                  <span></span>
+                  <span></span>
+                  <span></span>
                 </div>
-            </nav>
+              </div>
+
+              <div class="navbar-menu" id="navMenu">
+                <div class="navbar-start is-dark"></div>
+
+                <div class="navbar-end">
+                  @if (Auth::guest())
+                  <a class="navbar-item" href="{{ route('login') }}">Login</a>
+
+                  @else
+                  <div class="navbar-item has-dropdown is-hoverable">
+                    <a class="navbar-link" href="#">{{ Auth::user()->name }}</a>
+
+                    <div class="navbar is-dark">
+                      <div class="conteiner">
+                        <div class="navbar-brand">
+                          <a class="navbar-item" href="{{ route('register') }}">Register </a>
+                        </div>
+                      </div>
+                    </div>
+                    
+                    <div class="navbar-dropdown">
+                      <a class="navbar-item" href="{{ route('logout') }}" onclick="event.preventDefault();document.getElementById('logout-form').submit();">
+                        Logout
+                      </a>
+
+
+                      <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                        {{ csrf_field() }}
+                      </form>
+                    </div>
+                  </div>
+                  @endif
+                </div>
+              </div>
+            </div>
+          </nav>
         </div>
       </div>
     </div>
